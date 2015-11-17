d5469f1b02983e8585d8ee19d6b4ae05bfb3f1bc
emilyperegrine <tomfperegrine@googlemail.com>
2015-11-17 17:52:45 +0000

    Added random word if no word is specified

--- routes/api.js
+++ routes/api.js
@@ -4,29 +4,25 @@ var https = require("https");
 module.exports = function (app) {
 	app.get('/api/define', function (req, res) {
 		word = req.query.word;
-		if (word) {
-			https.get({
-				headers: {
-					"X-Mashape-Key": "zgaISKyNoImsha1507SORFsCpzATp1Jp2HYjsnUSEjhIFJHbsJ"
-				},
-				"hostname": "wordsapiv1.p.mashape.com",
-				"path": "/words/" + word 
-			}, function(response) {
-			  var body = '';
-			  response.on('data', function(chunk) {
-			    body += chunk;
-			  });
-			  response.on('end', function() {
-			  	console.log("Got response: " + response.statusCode);
-				  res.setHeader('Content-Type', 'application/json');
-				  res.send(body);
-			  });
-			}).on('error', function(e) {
-			  console.log("Got error: " + e.message);
-			  res.json(e);
-			});
-		} else {
-			res.send("Word not specified");
-		}
+		https.get({
+			headers: {
+				"X-Mashape-Key": "zgaISKyNoImsha1507SORFsCpzATp1Jp2HYjsnUSEjhIFJHbsJ"
+			},
+			"hostname": "wordsapiv1.p.mashape.com",
+			"path": "/words/" + (word ? word : "?random=true")
+		}, function(response) {
+		  var body = '';
+		  response.on('data', function(chunk) {
+		    body += chunk;
+		  });
+		  response.on('end', function() {
+		  	console.log("Got response: " + response.statusCode);
+			  res.setHeader('Content-Type', 'application/json');
+			  res.send(body);
+		  });
+		}).on('error', function(e) {
+		  console.log("Got error: " + e.message);
+		  res.json(e);
+		});
 	});
 };
--- routes/definition.js
+++ routes/definition.js
@@ -2,31 +2,27 @@ var http = require("http");
 module.exports = function (app) {
 	app.get('/define', function (req, res) {
 		word = req.query.word;
-		if (word) {
-			http.get({
-				"hostname": "localhost",
-				"path": "/api/define?word=" + word,
-				port: 3000
-			}, function(response) {
-			  var body = '';
-			  response.on('data', function(chunk) {
-			    body += chunk;
-			  });
-			  response.on('end', function() {
-			  	console.log("Got response: " + response.statusCode);
-			  	try {
-			  		body = JSON.parse(body);
-				  	res.render("definition.nunjucks", {word: word, definitions: body.results});
-			  	} catch (e) {
-			  		res.render("definition.nunjucks", {word: word, definitions: []});
-			  	}
-			  });
-			}).on('error', function(e) {
-			  console.log("Got error: " + e.message);
-			  res.render("message.nunjucks",{message: e.message});
-			});
-		} else {
-			res.render("message.nunjucks",{message: "Word not specified"});
-		}
+		http.get({
+			"hostname": "localhost",
+			"path": "/api/define" + (word ? "?word=" + word : ""),
+			port: 3000
+		}, function(response) {
+		  var body = '';
+		  response.on('data', function(chunk) {
+		    body += chunk;
+		  });
+		  response.on('end', function() {
+		  	console.log("Got response: " + response.statusCode);
+		  	try {
+		  		body = JSON.parse(body);
+			  	res.render("definition.nunjucks", {word: body.word, definitions: body.results});
+		  	} catch (e) {
+		  		res.render("definition.nunjucks", {word: body.word, definitions: []});
+		  	}
+		  });
+		}).on('error', function(e) {
+		  console.log("Got error: " + e.message);
+		  res.render("message.nunjucks",{message: e.message});
+		});
 	});
 };
